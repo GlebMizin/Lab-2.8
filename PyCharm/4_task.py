f2581504ad6bcca81f4b4b91a206d8f8c9171906
GlebMizin <112752101+GlebMizin@users.noreply.github.com>
2022-12-03 09:40:02 +0300

    Update 4_task.py

--- PyCharm/4_task.py
+++ PyCharm/4_task.py
@@ -18,13 +18,14 @@ def str_to_int(num):
 
 
 def print_int(num):
-    print(num)
+    print(num, type(num))
 
 
 if __name__ == "__main__":
     ent_num = get_input()
+    print(ent_num, type(ent_num))
     if test_input(ent_num):
         str_to_int(ent_num)
-        print_int(ent_num)
+        print_int(str_to_int(ent_num))
     else:
-        print("Cant use 'str_to_int'")
+        print(f"Cant use 'str_to_int' for {ent_num}")
